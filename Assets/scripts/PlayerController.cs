eba169614db5f2c2c30548b1dfeac364cffdcd49
xXchelikXx <162018732+xXchelikXx@users.noreply.github.com>
2025-09-29 22:00:32 +0300

    добавил прыжок и ходьбу и физику типо пон

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horiz * moveSpeed, rb.velocity.y);
+
+        if (horiz > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (horiz < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+    
+    // коллизии
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = false;
+        }
+
+    }
+}
