1ef5525ba0485e9ffbea0f9d6d6fd14a3aaaffdf
xXchelikXx <162018732+xXchelikXx@users.noreply.github.com>
2025-10-04 16:16:13 +0300

    onimochki_chinozes

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -8,31 +8,19 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 5f;
     private Rigidbody2D rb;
     public bool isGrounded;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horiz = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(horiz * moveSpeed, rb.velocity.y);
-
-        if (horiz > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        if (horiz < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+        Move();
+        Jump();
     }
     
     // коллизии
@@ -50,5 +38,30 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
 
+    }
+
+    private void Move()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horiz * moveSpeed, rb.velocity.y);
+        anim.SetBool("walk", horiz != 0);
+
+
+        if (horiz > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (horiz < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+    }
+    private void Jump()
+    {
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
